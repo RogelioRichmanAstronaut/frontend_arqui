0ac2f1ced56f5414460bd9af85e61c1b09a6f362
DavidVargas-Ctrl <dafelipe0410@gmail.com>
2025-11-18 18:20:18 -0500

    Interfaces de paquetes

---
+++ lib/types/packages.ts
@@ -0,0 +1,30 @@
+export interface RoomType {
+  habitacion_id: string;
+  tipo: string;
+  disponibilidad: string;
+  codigo_tipo_habitacion: string;
+  precio: number;
+  servicios_habitacion: string[];
+}
+
+export interface Hotel {
+  hotel_id: string;
+  nombre: string;
+  categoria_estrellas: number;
+  ciudad: string;
+  direccion: string;
+  servicios_hotel: string[];
+  fotos: string[];
+  habitaciones: RoomType[];
+}
+
+export interface Package {
+  title: string;
+  stars: number;
+  includes: string;
+  price: number;
+  displayPrice: string;
+  airline: string;
+  hasBreakfast: boolean;
+  hotel?: Hotel;
+}
